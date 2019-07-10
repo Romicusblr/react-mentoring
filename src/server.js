3116ab10e0ddb4fe4e45f76ca1984ee07cffae55
Raman <raman.mastyka@gmail.com>
2019-07-10 23:20:54 +0300

    extract port to const, log to console running port

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
+const PORT = 8000;
 app.use(express.static("public"));
 
 const server = require("http").createServer(app);
 
-server.listen(8000);
+server.listen(PORT, () => {
+    console.log("server is running on port %s", PORT);
+});
