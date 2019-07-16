a198d03e6b85f42be09f0a7544c5d26d98290887
Raman <raman.mastyka@gmail.com>
2019-07-16 14:31:35 +0300

    split webpack config to prod and dev

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,5 @@
-const path = require("path");
-
-module.exports = {
-    entry: "./src/index.js",
-    output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "public")
-    },
-    devServer: {
-        contentBase: path.join(__dirname, "public"),
-        port: 8000
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules)/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: [  "@babel/preset-env", "@babel/preset-react"]
-                    }
-                }
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    {loader: "style-loader"},
-                    {loader: "css-loader"}
-                ]
-            },
-            {
-                test: /\.(png|jpg)$/,
-                use: {
-                    loader: "url-loader"
-                },
-            }
-        ]
-    }
+module.exports = env => {
+    const mode = Object.keys(env)[0] || "dev";
+    const configPath = `./webpack/webpack.${mode}.js`;
+    return require(configPath);
 }
