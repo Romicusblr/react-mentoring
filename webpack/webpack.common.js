2a081b42406308b1bdd47ec73074b6f9f8e3d1ee
Raman <raman.mastyka@gmail.com>
2019-07-18 18:13:39 +0300

    fix regex for js and jsx

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -24,7 +24,7 @@ module.exports = {
   ],
   module: {
     rules: [{
-      test: /\.js(x)?$/,
+      test: /\.jsx?$/,
       exclude: /(node_modules)/,
       use: {
         loader: 'babel-loader',
