90d4ac65e04cc32982daea84a32c38e1aec5c0a3
Raman <raman.mastyka@gmail.com>
2019-07-25 18:14:46 +0300

    specify naming of classes in css modules

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -54,7 +54,9 @@ module.exports = {
       {
         loader: 'css-loader',
         options: {
-          modules: true,
+          modules: {
+            localIdentName: process.env.NODE_ENV === 'development' ? '[folder]__[local]' : '[local]__[hash:base64:5]',
+          },
         },
       },
       ],
