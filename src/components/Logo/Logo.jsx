0c7f3cee0e64b4e2107d08dc5fd821a1ab414d3d
Raman <raman.mastyka@gmail.com>
2019-07-26 15:56:22 +0300

    add Logo component

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import style from './style.module.css';
+
+function Logo() {
+  return (
+    <h3 className={style.logo}>
+      <strong>netflix</strong>
+      roulette
+    </h3>
+  );
+}
+
+export default Logo;
