650a4c148062981339188b8bd485844b949d8a06
Raman <raman.mastyka@gmail.com>
2019-07-28 00:50:03 +0300

    add fakeData, colors

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import Header from '../Header';
+import SearchBody from '../SearchBody';
 import './App.css';
+import data from '../fakeMovies';
 
 const App = () => (
-  <Header />
+  <div>
+    <Header />
+    <SearchBody data={data.data} />
+  </div>
 );
 
 export default App;
