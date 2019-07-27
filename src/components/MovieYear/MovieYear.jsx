c344526bd800cfd1b9a8b5713b3b9eb1f66b7888
Raman <raman.mastyka@gmail.com>
2019-07-27 18:34:15 +0300

    add movieYear component

---
+++ src/components/MovieYear/MovieYear.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import style from './style.module.css';
+
+function MovieYear({ year }) {
+  return <span className={style.movieYear}>{year}</span>;
+}
+
+MovieYear.propTypes = {
+  year: PropTypes.number,
+};
+
+MovieYear.defaultProps = {
+  year: '',
+};
+
+export default MovieYear;
