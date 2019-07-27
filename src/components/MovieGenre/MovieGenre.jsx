d0fded945d9a5a0d7b36ec2dba9ae8b487c080b6
Raman <raman.mastyka@gmail.com>
2019-07-27 19:23:56 +0300

    add movieGenre component

---
+++ src/components/MovieGenre/MovieGenre.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import style from './style.module.css';
+
+function MovieGenre({ genres }) {
+  return <span className={style.movieGenre}>{genres.slice(2).join(' & ')}</span>;
+}
+
+MovieGenre.propTypes = {
+  genres: PropTypes.arrayOf(PropTypes.string),
+};
+
+MovieGenre.defaultProps = {
+  genres: [],
+};
+
+export default MovieGenre;
