34b6feb6aa8c2601874e3e49ae18851c4bd7cded
Raman <raman.mastyka@gmail.com>
2019-07-28 14:04:59 +0300

    beautify

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../Header';
 import Main from '../Main';
+import Footer from '../Footer';
 import './App.css';
 import fakeData from '../fakeMovies';
 
@@ -8,6 +9,7 @@ const App = () => (
   <React.Fragment>
     <Header />
     <Main data={fakeData.data} />
+    <Footer />
   </React.Fragment>
 );
 
