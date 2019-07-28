110734d1e769060e69fc4d705a9a36e3869473c4
Raman <raman.mastyka@gmail.com>
2019-07-28 14:04:43 +0300

    add footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Logo from '../Logo';
+import style from './style.module.css';
+
+const Footer = () => (
+  <footer className={style.footer}>
+    <Logo />
+  </footer>
+);
+
+export default Footer;
