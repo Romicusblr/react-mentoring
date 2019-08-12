56591d191721dc9c57a19e62d448299712b1d6d4
Raman <raman.mastyka@gmail.com>
2019-08-13 01:13:23 +0300

    add api

---
+++ src/api/index.js
@@ -0,0 +1,14 @@
+import querystring from 'querystring';
+
+const baseUrl = 'https://reactjs-cdp.herokuapp.com';
+
+export default {
+  getMovies(params) {
+    return fetch(`${baseUrl}/movies?${querystring.stringify(params)}`)
+      .then(res => res.json());
+  },
+  getMovie(id) {
+    return fetch(`${baseUrl}/movies?${id}`)
+      .then(res => res.json());
+  },
+};
