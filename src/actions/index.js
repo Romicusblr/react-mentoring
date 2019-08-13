6b900a05310dc7c27cbb5902e49ea55fac86ff05
Raman <raman.mastyka@gmail.com>
2019-08-14 00:01:12 +0300

    add reducer, action and store examples

---
+++ src/actions/index.js
@@ -0,0 +1,48 @@
+import api from '../api';
+
+const moviesRequested = () => ({
+  type: 'FETCH_MOVIES_REQUEST',
+});
+
+const moviesLoaded = movies => ({
+  type: 'FETCH_MOVIES_SUCCESS',
+  payload: movies,
+});
+
+const moviesError = error => ({
+  type: 'FETCH_MOVIES_FAILURE',
+  payload: error,
+});
+
+const movieRequested = () => ({
+  type: 'FETCH_MOVIE_REQUEST',
+});
+
+const movieLoaded = movie => ({
+  type: 'FETCH_MOVIE_SUCCESS',
+  payload: movie,
+});
+
+const movieError = error => ({
+  type: 'FETCH_MOVIE_FAILURE',
+  payload: error,
+});
+
+const fetchMovies = () => params => (dispatch) => {
+  dispatch(moviesRequested());
+  api.getMovies(params)
+    .then(data => dispatch(moviesLoaded(data)))
+    .catch(err => dispatch(moviesError(err)));
+};
+
+const fetchMovie = () => id => (dispatch) => {
+  dispatch(movieRequested());
+  api.getMovie(id)
+    .then(data => dispatch(movieLoaded(data)))
+    .catch(err => dispatch(movieError(err)));
+};
+
+export default {
+  fetchMovies,
+  fetchMovie,
+};
---
+++ src/reducers/index.js
@@ -0,0 +1,61 @@
+export default (state, action) => {
+  if (state === undefined) {
+    return {
+      movies: null,
+      currMovie: null,
+      loading: true,
+      error: null,
+    };
+  }
+
+  switch (action.type) {
+    case 'FETCH_MOVIES_REQUEST':
+      return {
+        ...state,
+        movies: [],
+        loading: true,
+        error: null,
+      };
+
+    case 'FETCH_MOVIES_SUCCESS':
+      return {
+        ...state,
+        movies: action.payload,
+        loading: false,
+        error: null,
+      };
+
+    case 'FETCH_MOVIES_FAILURE':
+      return {
+        ...state,
+        movies: [],
+        loading: false,
+        error: action.payload,
+      };
+    case 'FETCH_MOVIE_REQUEST':
+      return {
+        ...state,
+        movie: null,
+        loading: true,
+        error: null,
+      };
+
+    case 'FETCH_MOVIE_SUCCESS':
+      return {
+        ...state,
+        movie: action.payload,
+        loading: false,
+        error: null,
+      };
+
+    case 'FETCH_MOVIE_FAILURE':
+      return {
+        ...state,
+        movie: null,
+        loading: false,
+        error: action.payload,
+      };
+    default:
+      return state.bookList;
+  }
+};
