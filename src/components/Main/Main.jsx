6ed1d786504e85a2eb0ecc0e161ab802839a0a7e
Raman Mastyka <mastroma@mail.ru>
2019-08-14 22:50:44 +0300

    delete fake data

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -5,26 +5,31 @@ import SearchBody from '../SearchBody';
 import MainHeader from '../MainHeader';
 import style from './Main.module.css';
 
-const Main = ({ data, className, ...other }) => (
-  <main
-    className={classNames(style.main, className)}
-    {...other}
-  >
-    <MainHeader quantity={data.total} />
-    <SearchBody movies={data.data} />
-  </main>
-);
+const Main = ({ data, className, ...other }) => {
+  if (!data) return null;
+  return (
+    <main
+      className={classNames(style.main, className)}
+      {...other}
+    >
+      <MainHeader quantity={data.total} />
+      <SearchBody movies={data.data} />
+    </main>
+  );
+};
+
 
 Main.propTypes = {
   data: PropTypes.shape({
     data: PropTypes.arrayOf(PropTypes.objectOf),
     total: PropTypes.number,
-  }).isRequired,
+  }),
   className: PropTypes.string,
 };
 
 Main.defaultProps = {
   className: '',
+  data: null,
 };
 
 export default Main;
