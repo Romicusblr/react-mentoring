9e88bcebd6a7dde9a896685768385fe1798adb07
Raman Mastyka <roman.mastyko@digiforest.io>
2019-08-23 15:53:59 +0300

    start to implement routing

---
+++ src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+import MainContainer from '../../components/Main/Main.container';
+
+const MainPage = () => (
+  <>
+    <Header />
+    <MainContainer />
+    <Footer />
+  </>
+);
+
+export default MainPage;
---
+++ src/pages/MoviePage/MoviePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import HeaderMovieDetails from '../../components/HeaderMovieDetails';
+import Footer from '../../components/Footer';
+import MainContainer from '../../components/Main/Main.container';
+
+const MoviePage = () => (
+  <>
+    <HeaderMovieDetails />
+    <MainContainer />
+    <Footer />
+  </>
+);
+
+export default MoviePage;
