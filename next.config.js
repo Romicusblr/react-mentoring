028dc88d72db0d5069e1b583120fb1801e3d3f5a
Raman <raman.mastyka@gmail.com>
2019-09-23 23:39:26 +0300

    add config

--- next.config.js
+++ next.config.js
@@ -1,20 +1,13 @@
-// next.config.js
 const withCSS = require('@zeit/next-css');
 
 module.exports = withCSS({
   cssModules: true,
-  // cssLoaderOptions: {
-  //   importLoaders: 1,
-  //   modules: {
-  //     localIdentName: '[path][name]__[local]--[hash:base64:5]',
-  //   },
-  // },
-  // {
-  //   test: /\.(png|jpg)$/,
-  //   use: {
-  //     loader: 'url-loader',
-  //   },
-  // },
+  cssLoaderOptions: {
+    importLoaders: 1,
+    modules: {
+      localIdentName: '[path][name]__[local]--[hash:base64:5]',
+    },
+  },
   webpack: (config, options) => {
     config.module.rules.push({
       test: /\.(png|jpg)$/,
